2f3e6e279ff8343b7cf43eff5ec0d122603b1a10
Sergei Roik <roiksg@mail.ru>
2019-12-21 17:52:39 +0300

    rename

---
+++ src/main/java/com/example/website/repository/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.website.repository;
+
+import com.example.website.model.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
